d831670f05698fec6f6e2bfda1b0604f01c19c79
Connor Mills <connor@virtualized.dev>
2026-03-05 16:38:22 -0800

    Created main menu with api

--- core.lua
+++ core.lua
@@ -1,12 +1,70 @@
 SPDRN = SMODS.current_mod
 
-sendDebugMessage(SPDRN.id, SPDRN.id)
+-----------------------------
+-- CORE FUNCTIONS
+-----------------------------
+
+function SPDRN.sendDebugMessage(msg)
+	sendDebugMessage(msg, SPDRN.id)
+end
+
+function SPDRN.sendWarnMessage(msg)
+	sendWarnMessage(msg, SPDRN.id)
+end
+
+function SPDRN.load_spdrn_file(file)
+	local chunk, err = SMODS.load_file(file, SPDRN.id)
+	if chunk then
+		local ok, func = pcall(chunk)
+		if ok then
+			return func
+		else
+			SPDRN.sendWarnMessage('Failed to process file: ' .. func)
+		end
+	else
+		SPDRN.sendWarnMessage('Failed to find or compile file: ' .. tostring(err))
+	end
+	return nil
+end
+
+function SPDRN.load_spdrn_dir(directory, recursive)
+	recursive = recursive or false
+
+	local dir_path = SPDRN.path .. '/' .. directory
+	local items = NFS.getDirectoryItemsInfo(dir_path)
+
+	for _, item in ipairs(items) do
+		local path = directory .. '/' .. item.name
+		SPDRN.sendDebugMessage('Loading item: ' .. path)
+		if item.type ~= 'directory' then
+			SPDRN.load_spdrn_file(path)
+		elseif recursive then
+			SPDRN.load_spdrn_dir(path, recursive)
+		end
+	end
+end
+
+-----------------------------
+-- FILE LOADING
+-----------------------------
+
+SPDRN.load_spdrn_dir('ui')
+
+-----------------------------
+-- MP REGISTER
+-----------------------------
+
+SPDRN.mp_config = {
+	id = SPDRN.id,
+	name = 'Speedrun',
+	colour = G.C.GREEN,
+	main_menu_ui = SPDRN.create_main_menu_ui,
+}
 
 MPAPI.on_loaded(function()
-    MPAPI.connect({
-        api_url = "http://localhost:8788",
-        mqtt_broker = "localhost",
-        mqtt_port = 1883,
-        mqtt_secure = false,
-    })
+	MPAPI.register_mod(SPDRN.mp_config)
+
+	MPAPI.on_connection_state_change(function()
+		SPDRN.update_main_menu_buttons()
+	end)
 end)
---
+++ ui/main_menu.lua
@@ -0,0 +1,135 @@
+-- Forward declarations for helper functions
+local create_buttons
+
+-----------------------------
+-- STATE VARIABLES
+-----------------------------
+
+local _find_game_button
+local _create_lobby_button
+local _join_by_code_button
+local _join_from_clipboard_button
+local _buttons_initialized = false
+
+local _connected = function()
+	return MPAPI.is_connected() == 'connected'
+end
+
+-----------------------------
+-- UI FUNCTIONS
+-----------------------------
+
+SPDRN.create_main_menu_ui = function()
+	create_buttons()
+
+	return {
+		n = G.UIT.ROOT,
+		config = { align = 'cm', colour = G.C.CLEAR },
+		nodes = {
+			{
+				n = G.UIT.C,
+				config = { align = 'bm' },
+				nodes = {
+					{
+						n = G.UIT.R,
+						config = { align = 'cm', padding = 0.1, r = 0.1, emboss = 0.1, colour = G.C.L_BLACK, mid = true },
+						nodes = {
+							_find_game_button.node,
+							_create_lobby_button.node,
+							{
+								n = G.UIT.C,
+								config = { align = 'cm', padding = 0.1, r = 0.2, colour = G.C.BLACK },
+								nodes = {
+									{
+										n = G.UIT.C,
+										config = { align = 'cm', maxh = 1.4 },
+										nodes = {
+											{ n = G.UIT.T, config = { text = localize('b_join_lobby_cap'), scale = 0.45, colour = G.C.UI.TEXT_LIGHT, vert = true, maxh = 1.4 } },
+										},
+									},
+									{
+										n = G.UIT.C,
+										config = { align = 'cm', padding = 0.1 },
+										nodes = {
+											{ n = G.UIT.R, config = { align = 'cm' }, nodes = { _join_by_code_button.node } },
+											{ n = G.UIT.R, config = { align = 'cm' }, nodes = { _join_from_clipboard_button.node } },
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+end
+
+create_buttons = function()
+	if not _buttons_initialized then
+		_find_game_button = MPAPI.disableable_button({
+			id = 'spdrn_find_game',
+			button = 'spdrn_find_game',
+			colour = G.C.GREEN,
+			minw = 3.65,
+			minh = 1.55,
+			label = localize('b_find_game_cap'),
+			disabled_text = { localize('b_find_game_cap') },
+			scale = 0.7,
+			col = true,
+			enabled = false,
+		})
+		_create_lobby_button = MPAPI.disableable_button({
+			id = 'spdrn_create_lobby',
+			button = 'spdrn_create_lobby',
+			colour = G.C.GREEN,
+			minw = 3.65,
+			minh = 1.55,
+			label = localize('b_create_lobby_cap'),
+			scale = 0.7,
+			col = true,
+			enabled = _connected,
+		})
+		_join_by_code_button = MPAPI.disableable_button({
+			id = 'spdrn_join_lobby_by_code',
+			button = 'spdrn_join_lobby_by_code',
+			colour = G.C.RED,
+			minw = 3.65,
+			minh = 0.6,
+			label = { localize('b_by_code_cap') },
+			scale = 0.45,
+			enabled = _connected,
+		})
+		_join_from_clipboard_button = MPAPI.disableable_button({
+			id = 'spdrn_join_lobby_from_clipboard',
+			button = 'spdrn_join_lobby_from_clipboard',
+			colour = G.C.PURPLE,
+			minw = 3.65,
+			minh = 0.6,
+			label = { localize('b_from_clipboard_cap') },
+			scale = 0.45,
+			enabled = _connected,
+		})
+	end
+
+	_buttons_initialized = true
+end
+
+-----------------------------
+-- LOGIC FUNCTIONS
+-----------------------------
+
+SPDRN.update_main_menu_buttons = function()
+	if _buttons_initialized then
+		_find_game_button:update()
+		_create_lobby_button:update()
+		_join_by_code_button:update()
+		_join_from_clipboard_button:update()
+	end
+end
+
+G.FUNCS.spdrn_create_lobby = function() end
+
+G.FUNCS.spdrn_join_lobby_by_code = function() end
+
+G.FUNCS.spdrn_join_lobby_from_clipboard = function() end
